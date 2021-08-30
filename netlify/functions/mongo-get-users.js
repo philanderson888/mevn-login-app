f968a9b73f6283b6263f3f1a987988eb965e1bfb
philanderson888 <philanderson888@hotmail.com>
2021-08-30 05:04:47 +0100

    modify mongo.js to mongo-get-users.js

---
+++ netlify/functions/mongo-get-users.js
@@ -0,0 +1,46 @@
+// ./lambda_functions/pokemon.js
+
+const MongoClient = require("mongodb").MongoClient;
+
+const MONGODB_URI = `mongodb+srv://${process.env.VUE_APP_MONGO_DB_USERNAME}:${process.env.VUE_APP_MONGO_DB_PASSWORD}@cluster0.iilta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(`MONGODB_URI = ${MONGODB_URI}`)
+
+const DB_NAME = 'myFirstDatabase';
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const users = await db.collection("users").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(users),
+  };
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  return queryDatabase(db);
+};
